3fdb21c37185774e8f8a180047fdcd091a7a74f7
aqueelis <aqueel.kmit10@gmail.com>
2026-01-18 16:14:14 -0500

    Terraform code for ECS service and tasks

---
+++ modules/ecs-service/task_definition.tf
@@ -0,0 +1,27 @@
+resource "aws_ecs_task_definition" "this" {
+  family = var.service_name
+  requires_compatibilities = ["EC2"]
+  network_mode = "bridge"
+  cpu = var.cpu
+  memory = var.memory
+  execution_role_arn = var.task_execution_role_arn
+  task_role_arn = var.task_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name      = var.container_name
+      image     = var.container_image
+      cpu       = var.cpu
+      memory    = var.memory
+      essential = true
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+    tags = var.tags
+}
---
+++ modules/ecs-service/variables.tf
@@ -0,0 +1,52 @@
+variable "service_name" {
+  type = string
+}
+
+variable "cluster_id" {
+  type = string
+}
+
+variable "task_execution_role_arn" {
+  type = string
+}
+
+variable "task_role_arn" {
+  type = string
+}
+
+variable "container_name" {
+  type = string
+}
+
+variable "container_image" {
+  type = string
+}
+
+variable "container_port" {
+  type = number
+  default = 5000
+}
+
+variable "cpu" {
+  type    = number
+  default = 256
+}
+
+variable "memory" {
+  type    = number
+  default = 256
+}
+
+variable "desired_count" {
+  type    = number
+  default = 1
+}
+
+variable "target_group_arn" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
