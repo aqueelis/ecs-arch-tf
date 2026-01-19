092eb9137342d342d3883ffad816e00a1eceb157
aqueelis <aqueel.kmit10@gmail.com>
2026-01-19 15:40:29 -0500

    attaching Certificate by hardoding certificate ARN

--- modules/alb/listeners.tf
+++ modules/alb/listeners.tf
@@ -3,6 +3,23 @@ resource "aws_lb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
   default_action {
+    type             = "redirect"
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port = 443
+  protocol = "HTTPS"
+
+  ssl_policy = "ELBSecurityPolicy-2016-08"
+  certificate_arn = "arn:aws:acm:us-east-1:456956234448:certificate/9ace21a4-0aac-4acd-82f1-099d5cb7f150"
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.this.arn
   }
